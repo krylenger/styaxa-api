651908d8436d8910870e36ef529b1fed70976362
krylenger <krylenger@gmail.com>
2020-06-17 20:12:43 +0800

    fix

--- server.js
+++ server.js
@@ -25,7 +25,8 @@ app.post('/paymentConfirmation', (req, res) => {
     db('paymentlogs').insert({
         name: notification_type,
         amount: amount,
-        sha1_hash: sha1_hash
+        sha1_hash: sha1_hash,
+        date_time: datetime
     }).then(console.log)
     console.log(req.body);
     res.status(200).json('working')
